6c69b885791eacaa99ef940c62b51fa015c7abb2
Tatiana Bartelová <xbartel@mail.muni.cz>
2026-04-23 19:18:24 +0200

    KAN-54: fix link to component library

---
+++ apps/web/src/i18n/index.ts
@@ -0,0 +1,18 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import en from "./locales/en.json";
+import cs from "./locales/cs.json";
+import sk from "./locales/sk.json";
+
+i18n.use(initReactI18next).init({
+    resources: {
+        en: { translation: en },
+        cz: { translation: cs },
+        sk: { translation: sk },
+    },
+    lng: "en",
+    fallbackLng: "en",
+    interpolation: { escapeValue: false },
+});
+
+export default i18n;
